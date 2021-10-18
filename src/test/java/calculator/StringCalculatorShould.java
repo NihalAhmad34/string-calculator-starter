748de9fb885bda12f5f6114dcc0da15fe1edca64
NihalAhmad34 <89202481+NihalAhmad34@users.noreply.github.com>
2021-10-18 10:48:10 +0530

    Update StringCalculatorShould.java

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -17,4 +17,9 @@ class StringCalculatorShould {
         StringCalculator stringCalculator = new StringCalculator();
         assertEquals(1, stringCalculator.add("1"));
     }
+    
+    @Test
+	public void testEmptyString() {
+		assertEquals(0, Calculator.add(""));
+	}
 }
